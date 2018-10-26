d533686cf15f0b5bde909935794d9831550199aa
vinod g chandaliya <vinodgchandaliya@gmail.com>
2018-10-26 17:28:28 +0530

    able to fetch user id and password from login form

---
+++ src/pages/login/login.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { NavController, NavParams } from 'ionic-angular';
+
+@Component({
+  selector: 'page-login',
+  templateUrl: 'login.html',
+})
+export class LoginPage {
+userid:string;
+password:string;
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad LoginPage');
+  }
+
+  login(){
+    console.log(this.userid, this.password)
+  }
+
+}
